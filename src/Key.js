09f590e6dba8d1c63aa8300e5b1867394e2f4abc
Hervé Claden <hervecladen@gmail.com>
2019-04-18 19:41:12 -0400

    Multi-touch added

--- src/Key.js
+++ src/Key.js
@@ -1,21 +1,41 @@
-import React from 'react';
+import React from "react";
 
-const Key = ({syllable}) => {
-    const playNote = () => {
-        var audio = new Audio(`/content/${syllable}.mp3`);
-        audio.volume = 0.2;
-        audio.currentTime = 0;
-        audio.play();
-        setTimeout(() => {
-            audio.pause();
-            audio.currentTime = 0;
-        }, 5500);
-    };
-    return(        
-        <button className={syllable.includes("♯") ? "sharp" : "syllable"} onClick={playNote}>
-            <p>{syllable}</p>
-        </button>
-    );
-}
+const Key = ({ syllable }) => {
+  let isTouching = false;
+
+  const playNote = () => {
+    const audio = new Audio(`/content/${syllable}.mp3`);
+    audio.currentTime = 0;
+    audio.volume = 0.1;
+    audio.play();
+    setTimeout(() => {
+      audio.pause();
+      audio.currentTime = 0;
+    }, 5500);
+  };
+
+  const onMouse = () => {
+    if (!isTouching) {
+      playNote();
+    } else {
+      isTouching = false;
+    }
+  };
+
+  const onTouch = () => {
+    playNote();
+    isTouching = true;
+  };
+
+  return (
+    <button
+      className={syllable.includes("♯") ? "note sharp" : "note syllable"}
+      onMouseDown={onMouse}
+      onTouchStart={onTouch}
+    >
+      <p>{syllable}</p>
+    </button>
+  );
+};
 
 export default Key;
