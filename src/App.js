87a722730047b4e278bd0f9b23e4fc462329af0a
Hervé Claden <hervecladen@gmail.com>
2019-04-24 02:38:24 -0400

    Song samples (colors)

--- src/App.js
+++ src/App.js
@@ -18,41 +18,46 @@ const App = () => {
     z: "Do5", x: "Re5", c: "Mi5", v: "Fa5", b: "Sol5", n: "La5", m: "Si5",
   }
 
-
   const timeouts = [];
-  const audio = new Audio();
-  const audioR = new Audio();
-  const audioL = new Audio();
-  audio.volume = 0.2;
-  audioR.volume = 0.2;
-  audioL.volume = 0.2;
   let time;
   
   document.addEventListener("keypress", function(e) {
     if (emulatedKeys.hasOwnProperty(e.key)) {
+      const audio = new Audio();
+      audio.volume = 0.2;
       audio.src = `/content/${emulatedKeys[e.key]}.mp3`;
-      audio.currentTime = 0;
       audio.play();
       setTimeout(() => {
         audio.pause();
-        audio.currentTime = 0;
       }, 5500);
     }
   });
 
   const playSong = (rightHand, leftHand) => {
     clearTimeouts();
-    playNotes(rightHand, audioR)
+    if (rightHand !== undefined && rightHand.length !== 0) {
+      playNotes(rightHand, "lightseagreen");
+    }
     if (leftHand !== undefined && leftHand.length !== 0) {
-      playNotes(leftHand, audioL);
+      playNotes(leftHand, "crimson");
     }
   }
 
-  const playNotes = (notes, sound) => {
+  const playNotes = (notes, color) => {
     time = 0;
     notes.forEach(note => {
       time += note[1];
       timeouts.push(setTimeout(() => {
+        document.querySelectorAll('.note').forEach(n => {
+          if(n.firstElementChild.textContent == note[0]) {
+            n.style.backgroundImage = `linear-gradient(transparent, ${color})`;
+            setTimeout(() => {
+              n.style.backgroundImage = "none";
+            }, 250);
+          }
+        });
+        let sound = new Audio();
+        sound.volume = 0.2;
         sound.src = `/content/${note[0]}.mp3`;
         sound.play();
       }, time));
@@ -65,8 +70,8 @@ const App = () => {
     }
   }
 
-  const UNOwen = [
-    ["Re4", 0],["La4", 500],["Mi4", 500],["La4", 500],["Fa4", 500],["Sol4", 500],["La4", 250],["Sol4", 250],["Do5", 500],["Re5", 500],
+  const sample = [
+    ["Re4", 250],["La4", 500],["Mi4", 500],["La4", 500],["Fa4", 500],["Sol4", 500],["La4", 250],["Sol4", 250],["Do5", 500],["Re5", 500],
     ["La4", 250],["Mi5", 250],["Fa5", 250],["Mi5", 250],["Fa5", 250],["Mi5", 125],["Re5", 125],["Do5", 250],["La4", 250],["Do5", 250],
     ["Sol4", 250],["La4", 250],["Fa4", 250],
     ["Re4", 1000],["La4", 500],["Mi4", 500],["La4", 500],["Fa4", 500],["Sol4", 500],["La4", 250],["Sol4", 250],["Do5", 500],["Re5", 500],
@@ -74,16 +79,38 @@ const App = () => {
   ];
 
   const introR = [
-    ["La4", 0],["Fa4", 500],["Sol♯4", 500],["Mi4", 500],["Si4", 500],["Sol♯4", 500],["Sol4", 500],["La♯4", 500],
+    ["La4", 250],["Fa4", 500],["Sol♯4", 500],["Mi4", 500],["Si4", 500],["Sol♯4", 500],["Sol4", 500],["La♯4", 500],
     ["La4", 500],["Fa4", 500],["Sol♯4", 500],["Mi4", 500],["Si4", 500],["Sol♯4", 500],["Sol4", 500],
     ["La4", 1000],["Fa4", 500],["Sol♯4", 500],["Mi4", 500],["Si4", 500],["Sol♯4", 500],["Sol4", 500],["La♯4", 500],
     ["La4", 500],["Fa4", 500],["Sol♯4", 500],["Mi4", 500],["Si4", 500],["Sol♯4", 500],["Sol4", 500]
   ];
   const introL = [
-    ["La3", 0],             ["Sol♯3", 1000],            ["Si3", 1000],              ["La♯3", 1000],
+    ["La3", 250],             ["Sol♯3", 1000],            ["Si3", 1000],              ["La♯3", 1000],
     ["La3", 1000],            ["Sol♯3", 1000],            ["Si3", 1000],              ["La♯3", 1000],
     ["La3", 1000],["La3", 500],["Sol♯3", 500],["Sol♯3", 500],["Si3", 500],["Si3", 500],["La♯3", 500],["La♯3", 500],
     ["La3", 500],["La3", 500],["Sol♯3", 500],["Sol♯3", 500],["Si3", 500],["Si3", 500],["La♯3", 500]
+  ];
+
+  const furR = [
+    ['Mi5', 250],['Re♯5',175],['Mi5',175],['Re♯5',175],['Mi5',175],['Si4',175],['Re5',175],['Do5',175],['La4',175],
+    ['Do4',525],['Mi4',175],['La4',175],['Si4',175],['Mi4',525],['La4',175],['Si4',175],['Do5',175],
+    ['Mi5', 1000],['Re♯5',175],['Mi5',175],['Re♯5',175],['Mi5',175],['Si4',175],['Re5',175],['Do5',175],['La4',175],
+    ['Do4',525],['Mi4',175],['La4',175],['Si4',175],['Mi4',525],['Do5',175],['Si4',175],['La4',175]
+  ];
+  const furL = [
+    ['La2',1650],['Mi3',175],['La3',175],['Mi2',700],['Mi3',175],['Sol♯3',175],
+    ['La2',3125],['Mi3',175],['La3',175],['Mi2',700],['Mi3',175],['Sol♯3',175]
+  ];
+
+  const rondoR = [
+    ['Si4',250],['La4',150],['Sol♯4',150],['La4',150],['Do5',150],
+    ['Re5',500],['Do5',150],['Si4',150],['Do5',150],['Mi5',150],
+    ['Fa5',500],['Mi5',150],['Re♯5',150],['Mi5',150],
+    ['Si5',150],['La5',150],['Sol♯5',150],['La5',150],['Si5',150],['La5',150],['Sol♯5',150],['La5',150],['Do6',150],
+    ['La5',750],['Do6',300],['Sol5',300],['La5',50],['Si5',50],
+    ['Fa♯5',300],['La5',0],['Mi5',300],['Sol5',0],['Fa♯5',300],['La5',0],['Sol5',300],['La5',50],['Si5',50],
+    ['Fa♯5',300],['La5',0],['Mi5',300],['Sol5',0],['Fa♯5',300],['La5',0],['Sol5',300],['La5',50],['Si5',50],
+    ['Fa♯5',300],['La5',0],['Mi5',300],['Sol5',0],['Re♯5',300],['Fa♯5',0],['Mi5',300]
   ];
 
   return (
@@ -91,8 +118,10 @@ const App = () => {
       {syllables.map((note, index) => (
         <Key syllable={note} key={index} />
       ))}
-      <button className="songs" onClick={() => playSong(introR, introL)}>Intro</button>
-      <button className="songs" onClick={() => playSong(UNOwen)}>U.N. Owen Was Her</button>
+      <button className="songs" onClick={() => playSong(furR, furL)}>Für</button>
+      <button className="songs" onClick={() => playSong(rondoR)}>Rondo</button>
+      {/* <button className="songs" onClick={() => playSong(sample)}>Sample</button> */}
+      {/* <button className="songs" onClick={() => playSong(introR, introL)}>Sample 2</button> */}
       <button className="stop" onClick={() => clearTimeouts()}>Pause</button>
     </div>
   );
